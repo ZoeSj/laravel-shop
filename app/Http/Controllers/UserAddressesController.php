dc76b93d65940e8d70e71f605b226b9dc63125ff
shengjia <shengj@anchnet.com>
2018-06-19 15:52:46 +0800

    新增收货地址

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserAddress;
 use Illuminate\Http\Request;
+use App\Http\Requests\UserAddressRequest;
 
 class UserAddressesController extends Controller
 {
@@ -15,5 +17,32 @@ class UserAddressesController extends Controller
         return view('user_addresses.index', [
             'addresses' => $request->user()->addresses,
         ]);
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('user_addresses.create_and_edit', ['address' => new UserAddress()]);
+    }
+
+    /**
+     * @param UserAddressRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(UserAddressRequest $request)
+    {
+        $request->user()->addresses()->create($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone',
+        ]));
+
+        return redirect()->route('user_addresses.index');
     }
 }
