18af138adcd5317ff8f36c5c63cbed52aa24dbab
shengjia <shengj@anchnet.com>
2018-06-19 13:48:26 +0800

    触发激活邮件

--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,10 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
+    Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');//注意：千万不要把这个路由放在 email_verified 中间件的路由组中。
+    Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');//不要把这个路由放在 email_verified 的路由组中。
     //开始
     Route::group(['middleware' => 'email_verified'], function () {
-        Route::get('/test', function () {
-            return 'your email is verified';
-        });
     });
     //end
 });
